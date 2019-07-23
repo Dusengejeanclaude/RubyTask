fe0935e1c04f9c463fa5f5df50fffd3088772026
dusengejeanclaude <dusengejeanclaude@gmail.com>
2019-07-23 15:18:27 +0100

    third commit

--- Sample.rb
+++ Sample.rb
@@ -1,53 +1,42 @@
 class Player 
- def hand
-   # Process of the following method is created in this.
-   # Puts the console into an input wait state, and outputs the value the player types from the console
+  def hand
     puts "Enter a  number between 0 and 2"
     player_number = gets.to_i
-    while player_number > 2 || player_number < 0 do
-        puts "Wrong number please try again:"
-        player_number = gets.to_i
-    end
+  while player_number > 2 || player_number < 0  do
+    puts "Wrong number please try again:"
+    player_number = gets.to_i
+  end
     puts "you choose #{player_number}"
     player_number
- end
+  end
 end
 
 class Enemy 
- def hand
-   # Create processing of the method that outputs the value of goo, choki, par at random in this
-   computer_choice = rand(0..2)
-   puts "I choose #{computer_choice}"
-   computer_choice
- end
+  def hand
+    computer_choice = rand(0..2)
+    puts "I choose #{computer_choice}"
+    computer_choice
+  end
 end
 
 class Janken 
-
- def pon(player_hand, enemy_hand)
-   # Create a method in it that will cause the player to hit the value you typed in and the one that Enemy randomly gave out, and output the result on the console
-   # At that time, when you enter values other than draw or Goo, Choki, Par, do another Janken.
-   # Show what the other party gave out of Goo, Choki, Par
-   
-   
-   difference = player_hand - enemy_hand
-   final_answer =(difference + 3)%3
-   if final_answer == 2
+  def pon(player_hand, enemy_hand)
+    difference = player_hand - enemy_hand
+    final_answer =(difference + 3)%3
+    if final_answer == 2
     puts "You are the winner"
-   elsif final_answer == 1
+    elsif final_answer == 1
     puts "You are the looser"
-   else 
+    else 
     puts "it's a draw. let's try again"
     player = Player.new
     enemy = Enemy.new
     janken = Janken.new
     janken.pon(player.hand, enemy.hand)
-   end 
+    end
+  end 
 end
-end 
-
 player = Player.new
 enemy = Enemy.new
 janken = Janken.new
-# The Janken method is invoked by the following description
-janken.pon(player.hand, enemy.hand,)
+janken.pon(player.hand, enemy.hand)
